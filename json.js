cfa69e00675fdf284ebaadfe41d5e711e1b90f91
user_name <alitarekkamal@gmail.com>
2018-04-28 13:21:19 +0200

    loop cart Error

--- json.js
+++ json.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
+  var info;
+  var cart;
   if ($("body").hasClass("home")){
     $.getJSON("product.json", function(data) {
         for (var name in data) {
+          datainfo = JSON.stringify(data[name]); // stringify it-->
+          //intro= JSON.parse(datainfo);
+          //console.log(intro);
           $(".items").append(
             "<div class='col-sm-4'>"+
               "<div class='panel panel-default'>"+
@@ -11,52 +16,45 @@ $(document).ready(function(){
                 "</div>"+
                 "<div class='panel-body'><img src='"+data[name].img+"' class='img-responsive' style='width:100%' alt='Image'></div>"+
                 "<div class='panel-footer'>"+
-                  "<div class='btn btn-success add'img='"+data[name].img+"' featuer='"+data[name].featuer+"' price='"+data[name].price+"' name='"+data[name].name +"' id='"+ data[name].id +"' style='margin-right:11%; margin-left:5%;'>Add to Cart <span class='glyphicon glyphicon-shopping-cart'></span></div>"+
+                  "<div class='btn btn-success add' info='"+ datainfo +"' style='margin-right:11%; margin-left:5%;'>Add to Cart <span class='glyphicon glyphicon-shopping-cart'></span></div>"+
                   "<div class='btn btn-info'>Show Product <span class='glyphicon glyphicon-eye-open'></span></div>"+
                 "</div></div></div>");
                 //--------------------- defined attr to use ------------------------
-              var id;
-              var img;
-              var price;
-              var name;
-              var featuer;
-              $(".add").click(function(){
-                img = $(this).attr('img');
-                id = $(this).attr('id');
-                price = $(this).attr('price');
-                name = $(this).attr('name');
-                featuer = $(this).attr('featuer');
-                  localStorage.setItem("id",id);
-                  localStorage.setItem("name", name);
-                  localStorage.setItem( "price",price);
-                  localStorage.setItem( "img",img);
-                  localStorage.setItem( "featuer",featuer);
-                });
+                    let items = [];
+                    $(".add").click(function() {
+
+                      var info = $(this).attr("info");
+                    items.push(info);
+                    localStorage.setItem("items", items);
+
+                    console.log(localStorage.getItem("items"));
+                    });
+
+
           }
       }, "json");
 
     }else if ( $("body").hasClass("cart")){
       $(document).ready(function(){
 
-          var setname = localStorage.name;
-          var setimg = localStorage.img;
-          var setprice = localStorage.price;
-          var setid = localStorage.id;
-          var setfeatuer = localStorage.featuer;
+          Data = localStorage.getItem("items");
+
+          for (var name in Data){
 
           $(".inputResult").append(
             '<div class="col-sm-3">'+
               '<div class="well">'+
-               '<p class="productName">'+setname+' </p>'+
-               '<img src="'+setimg+'" class="img-responsive">'+
+               '<p class="productName">'+Data+' </p>'+
+               '<img src="'+Data[name].img+'" class="img-responsive">'+
               '</div>'+
             '</div>'+
             '<div class="col-sm-9">'+
               '<div class="well">'+
-                '<h3> Price: '+setprice+' $</h3><h5><strong>Featuers:</strong> '+setfeatuer +' </h5>'+
+                '<h3> Price: '+Data[name].price+' $</h3><h5><strong>Featuers:</strong> '+Data[name].featuer +' </h5>'+
             '</div></div>'
           );
-          $(".totalcost").append(setprice+" $");
+          }
+          //$(".totalcost").append(expandData[name].price+" $");
       });
     }
 });
